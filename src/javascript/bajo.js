def02812c100fb4b05ecdb9b3176a295a14bb9d0
91am <killaek.info@gmail.com>
2023-12-03 20:42:12 +0100

    Aumento bajo

--- src/javascript/bajo.js
+++ src/javascript/bajo.js
@@ -16,3 +16,44 @@ function jsNota(frecuencia) {
     o.start(0);
     g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 1.5);
 }
+
+document.addEventListener('keydown', function (event) {
+    switch (event.key) {
+        case 'a':
+            jsNota(73.4162);
+            break;
+        case 'w':
+            jsNota(77.7817);
+            break;
+        case 's':
+            jsNota(82.4069);
+            break;
+        case 'e':
+            jsNota(87.3071);
+            break;
+        case 'd':
+            jsNota(92.4986);
+            break;
+        case 'r':
+            jsNota(97.9989);
+            break;
+        case 'f':
+            jsNota(103.8262);
+            break;
+        case 'g':
+            jsNota(110.0000);
+            break;
+        case 'y':
+            jsNota(116.5409);
+            break;
+        case 'h':
+            jsNota(123.4708);
+            break;
+        case 'u':
+            jsNota(130.8128);
+            break;
+        case 'j':
+            jsNota(138.5913);
+            break;
+    }
+});
