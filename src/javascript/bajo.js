11b72091eda0d69ae4261e8b834cc70cd785a729
91am <killaek.info@gmail.com>
2023-12-04 22:42:20 +0100

    Aumento bajo finalizado con responsive

--- src/javascript/bajo.js
+++ src/javascript/bajo.js
@@ -19,41 +19,128 @@ function jsNota(frecuencia) {
 
 document.addEventListener('keydown', function (event) {
     switch (event.key) {
-        case 'a':
-            jsNota(73.4162);
+        case '1':
+            jsNota(103, 8262);
             break;
-        case 'w':
+        case '2':
+            jsNota(110.0000);
+            break;
+        case '3':
+            jsNota(116.5409);
+            break;
+        case '4':
+            jsNota(123.4708);
+            break;
+        case '5':
+            jsNota(130.8128);
+            break;
+        case '6':
+            jsNota(138.5913);
+            break;
+        case '7':
+            jsNota(146.8324);
+            break;
+        case '8':
+            jsNota(155.5635);
+            break;
+        case '9':
+            jsNota(164.8138);
+            break;
+        case '0':
+            jsNota(174.6141);
+        //Notas de D 
+        case 'q':
             jsNota(77.7817);
             break;
-        case 's':
+        case 'w':
             jsNota(82.4069);
             break;
         case 'e':
             jsNota(87.3071);
             break;
-        case 'd':
+        case 'r':
             jsNota(92.4986);
             break;
-        case 'r':
+        case 't':
             jsNota(97.9989);
             break;
-        case 'f':
+        case 'y':
             jsNota(103.8262);
             break;
-        case 'g':
+        case 'u':
             jsNota(110.0000);
             break;
-        case 'y':
+        case 'i':
             jsNota(116.5409);
             break;
-        case 'h':
+        case 'o':
             jsNota(123.4708);
             break;
-        case 'u':
+        case 'p':
             jsNota(130.8128);
+
+        //Notas de A
+
+        case 'a':
+            jsNota(58.2705);
+            break;
+        case 's':
+            jsNota(61.7354);
+            break;
+        case 'd':
+            jsNota(65.4064);
+            break;
+        case 'f':
+            jsNota(69.2957);
+            break;
+        case 'g':
+            jsNota(73.4162);
+            break;
+        case 'h':
+            jsNota(77.7817);
             break;
         case 'j':
-            jsNota(138.5913);
+            jsNota(82.4069);
             break;
+        case 'k':
+            jsNota(87.3071);
+            break;
+        case 'l':
+            jsNota(92.4986);
+            break;
+        case ';':
+            jsNota(97.9989);
+
+        //Notas de E
+
+        case '<':
+            jsNota(43.6536);
+            break;
+        case 'z':
+            jsNota(46.2493);
+            break;
+        case 'x':
+            jsNota(48.9995);
+            break;
+        case 'c':
+            jsNota(51.9130);
+            break;
+        case 'v':
+            jsNota(55.0000);
+            break;
+        case 'b':
+            jsNota(58.2705);
+            break;
+        case 'n':
+            jsNota(61.7354);
+            break;
+        case 'm':
+            jsNota(65.4064);
+            break;
+        case 'l':
+            jsNota(69.2957);
+            break;
+        case ',':
+            jsNota(73.4162);
     }
 });
