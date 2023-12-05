408d6650e61a59480c1842e0a195fb21aa7aaf55
mava <mavafiza@gmail.com>
2023-12-05 04:13:45 +0100

    Terminando Responsive Piano, Carrusel de musicos, home mobile y muchos retoques

---
+++ slider/src/javascript/slider.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    center: true,
+    loop: true,
+    nav: true,
+    navText: ["<span class='icon icon-arrow-left7'></span>", 
+                "<span class='icon icon-arrow-right7'></span>"],
+    items: 1,
+    responsive: {
+        768: {
+            items: 3,
+        }
+    }
+});
